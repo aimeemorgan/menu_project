7073e03f812232629e8660f654f16145dbf68574
Aimee Morgan <aimeelmorgan@gmail.com>
2013-07-23 14:13:08 -0700

    completed data model, import script

--- data_import.py
+++ data_import.py
@@ -1,55 +1,79 @@
-
 import model
 import csv
 import datetime
 
 
 def load_menus_and_restaurants(session):
-    with open('./ref_docs/Menu.csv') as csvfile:
+    with open('./ref_docs/menu_test') as csvfile:
         menureader = csv.reader(csvfile, delimiter=",")
+        rest_id = 1
         for row in menureader:  # format date
-            #     title = row[1]
-            #     title = title.decode("latin-1")
-            #     r_date = row[2].split("-")
-            #     months = {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06", "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"}
-            #     r_date_datetime = datetime.date(int(r_date[2]), int(months[r_date[1]]), int(r_date[0]))
-            new_menu = model.Menu(id=int(row[0]), date=row[11],
-                                  currency=row[14], occasion=row[7],
-                                  sponsor=row[2])
-            session.add(new_menu)
-            new_restaurant = model.Restaurant(name=row[13], location=row[5])
+            new_restaurant = model.Restaurant(name=row[13],
+                                            location=row[5].title().strip(";"),
+                                            id=rest_id)
             session.add(new_restaurant)
             session.commit()
+            if row[12] != '':
+                date_args = row[12].split('-')
+                date = datetime.date(int(date_args[0]),
+                                     int(date_args[1]),
+                                     int(date_args[2]))
+            else:
+                date = None
+            new_menu = model.Menu(id=int(row[0]),
+                                  date=date,
+                                  currency=row[14],
+                                  occasion=row[7].strip(";").title(),
+                                  sponsor=row[2].title(),
+                                  restaurant_id=rest_id)
+            session.add(new_menu)
+            session.commit()
+            rest_id += 1
 
 
 def load_items(session):
-    with open('./ref_docs/Dish.csv') as csvfile:
+    with open('./ref_docs/dishes_test.csv') as csvfile:
         itemreader = csv.reader(csvfile, delimiter=",")
-        for row in itemreader:  # put date for first and latest years in correct format
-            #     title = row[1]
-            #     title = title.decode("latin-1")
-            #     r_date = row[2].split("-")
-            #     months = {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06", "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"}
-            #     r_date_datetime = datetime.date(int(r_date[2]), int(months[r_date[1]]), int(r_date[0]))
-            new_item = model.Item(id=row[0], description=row[x], 
-                                  first_year=row[x], latest_year=row[x],
-                                  low_price=float(row[x]),
-                                  high_price=float(row[x]))
+        for row in itemreader:
+            new_item = model.Item(id=int(row[0]),
+                                  description=row[1].strip("\"").title(),
+                                  first_year=datetime.date(int(row[5]), 1, 1),
+                                  latest_year=datetime.date(int(row[6]), 1, 1),
+                                  low_price=float(row[7]),
+                                  high_price=float(row[8]))
             session.add(new_item)
             session.commit()
 
 
-def load_menusitems(session):
-    pass
+def load_menuitems(session):
+    ##### read menupages csv and create a dictionary w menu page IDs as keys, and menu IDs as values
+    #### them get penu page IDs while reading the menuitems csv below, use it to look up the menu ID
+    ### and feed it into the database as a argument below
 
-def load restaurantsmenus(session):
-    pass
+    # build menu_page_id to menu_id dictionary
+    with open ('./ref_docs/MenuPage.csv') as csvfile:
+        menu_ids = {}
+        itemreader = csv.reader(csvfile, delimiter=",")
+        for row in itemreader:
+            menu_ids[row[0]] = menu_ids[1]
+
+    with open('./ref_docs/menuitem_test.csv') as csvfile:
+        itemreader = csv.reader(csvfile, delimiter=",")
+        for row in itemreader:
+            menu_id = menu_ids[row[1]]
+            new_menuitem = model.menuitems(id=int(row[0]),
+                                  item_id=row[4],
+                                  menu_id=menu_id,
+                                  price=float(row[2]))
+            session.add(new_menuitem)
+            session.commit()
+
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    load_menus_and_restaurants(session)
+    #load_menus_and_restaurants(session)
     load_items(session)
 
 if __name__ == "__main__":
-    s= model.connect()
+    s = model.session
     main(s)
--- model.py
+++ model.py
@@ -1,8 +1,9 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime, create_engine, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, Float, create_engine, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship, backref, scoped_session
 
-engine = create_engine("sqlite:///ratings.db", echo=False) # replace w postgres connection info
+engine = create_engine('postgresql+psycopg2://alm:password@localhost/nyplmenus')
+
 session = scoped_session(sessionmaker(bind=engine,
                                       autocommit=False,
                                       autoflush = False))
@@ -13,41 +14,50 @@ Base.query = session.query_property()
 ### Class declarations go here 
 
 
-class Menu(Base):  # menus
-    __tablename__ = "menus"
-
-    id = Column(Integer, primary_key=True)
-    date = Column(DateTime, nullable=True)
-    currency = Column(String(32), nullable=True)
-    occasion = Column(String(256), nullable=True)
-    sponsor = Column(String(256))
-
-    # rater = relationship("User", backref=backref("ratings", order_by=id))
-
-
-class Restaurant(Base):  # restaurants
+class Restaurant(Base):
     __tablename__ = "restaurants"
 
     id = Column(Integer, primary_key=True)
     name = Column(String(256), nullable=False)
     location = Column(String(256))
 
-    menus = relationship("Menu,", backref=backref("menus"))
-    # movie_ratings = relationship("Rating", backref=backref("users"))
+    # menus = relationship("Menu,", backref=backref("menus"))
 
 
-class Item(Base):  # items
+class Menu(Base):
+    __tablename__ = "menus"
+
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime, nullable=True)
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'), nullable=False)
+    currency = Column(String(32), nullable=True)
+    occasion = Column(String(256), nullable=True)
+    sponsor = Column(String(256))
+
+    # restaurant = relationship("Restaurant", backref=backref("restaurants"))
+
+
+class Item(Base):
     __tablename__ = "items"
 
     id = Column(Integer, primary_key=True)
     description = Column(String(512), nullable=False)
     first_year = Column(DateTime, nullable=True)
     latest_year = Column(DateTime, nullable=True)
-    low_price = Column(Number, nullable=True)
+    low_price = Column(Float, nullable=True)
     high_price = Column(Float, nullable=True)
 
-    # # use rating class as association object
-    # ratings = relationship("Rating", backref="movies")
+
+class MenuItems(Base):
+    __tablename__ = "menuitems"
+
+    id = Column(Integer, primary_key=True)
+    menu_id = Column(Integer, ForeignKey('menus.id'), nullable=False)
+    item_id = Column(Integer, ForeignKey('items.id'), nullable=False)
+    price = Column(Float, nullable=True)
+
+
+
 
 
 
